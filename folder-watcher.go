c353e5dfab4f0112625031ff560a181735331989
Simo-Pekka Kerkelä <simopekka.kerkela@spkerkela.com>
2023-01-12 19:40:26 +0200

    initial commit

---
+++ folder-watcher.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	folderToWatch := os.Args[1]
+	commands := os.Args[2:]
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				rest := append(commands[1:], event.Name)
+
+				log.Printf("executing %s %s", commands[0], strings.Join(rest, " "))
+				exec.Command(commands[0], rest...).Run()
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	// Add a path.
+	err = watcher.Add(folderToWatch)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Block main goroutine forever.
+	<-make(chan struct{})
+}
