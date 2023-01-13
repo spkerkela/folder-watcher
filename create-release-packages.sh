c924c2bf65bee80022999d2680c22118482459d6
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2023-01-13 17:39:16 +0200

    fix build.sh and create-release-package.sh to be less hardcoded

--- create-release-packages.sh
+++ create-release-packages.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-PROGRAM_NAME="backup-uploader"
+PROGRAM_NAME=$(basename "$PWD")
 
-tar -czf build/linux/amd64/$PROGRAM_NAME.tar.gz -C build/linux/amd64/ $PROGRAM_NAME
-tar -czf build/linux/arm/$PROGRAM_NAME.tar.gz -C build/linux/arm/ $PROGRAM_NAME
-tar -czf build/linux/386/$PROGRAM_NAME.tar.gz -C build/linux/386/ $PROGRAM_NAME
-tar -czf build/darwin/amd64/$PROGRAM_NAME.tar.gz -C build/darwin/amd64/ $PROGRAM_NAME
-tar -czf build/darwin/arm64/$PROGRAM_NAME.tar.gz -C build/darwin/arm64/ $PROGRAM_NAME
-tar -czf build/windows/amd64/$PROGRAM_NAME.tar.gz -C build/windows/amd64/ $PROGRAM_NAME.exe
+tar -czf build/linux/amd64/"$PROGRAM_NAME".tar.gz -C build/linux/amd64/ "$PROGRAM_NAME"
+tar -czf build/linux/arm/"$PROGRAM_NAME".tar.gz -C build/linux/arm/ "$PROGRAM_NAME"
+tar -czf build/linux/386/"$PROGRAM_NAME".tar.gz -C build/linux/386/ "$PROGRAM_NAME"
+tar -czf build/darwin/amd64/"$PROGRAM_NAME".tar.gz -C build/darwin/amd64/ "$PROGRAM_NAME"
+tar -czf build/darwin/arm64/"$PROGRAM_NAME".tar.gz -C build/darwin/arm64/ "$PROGRAM_NAME"
+tar -czf build/windows/amd64/"$PROGRAM_NAME".tar.gz -C build/windows/amd64/ "$PROGRAM_NAME".exe
