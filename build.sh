12986bf6255e5d88a1791acc1c47670b1074a378
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2023-01-13 17:12:55 +0200

    build on ci

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PROGRAM_NAME="backup-uploader"
+
+GOOS=windows GOARCH=amd64 go build -o build/windows/amd64/$PROGRAM_NAME.exe
+GOOS=darwin GOARCH=amd64 go build -o build/darwin/amd64/$PROGRAM_NAME
+GOOS=darwin GOARCH=arm64 go build -o build/darwin/arm64/$PROGRAM_NAME
+GOOS=linux GOARCH=amd64 go build -o build/linux/amd64/$PROGRAM_NAME
+GOOS=linux GOARCH=arm go build -o build/linux/arm/$PROGRAM_NAME
+GOOS=linux GOARCH=386 go build -o build/linux/386/$PROGRAM_NAME
